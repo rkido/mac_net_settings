99baf333d45803ab9ab16dc56d86ef4195e8b753
Ryan Kido <ryankido@yahoo.com>
2015-07-13 09:27:10 -1000

    add mac_tcp

---
+++ mac_tcp.py
@@ -0,0 +1,22 @@
+import os
+
+sudo = "sudo sysctl -w"
+ctl_var = [" net.inet.tcp.win_scale_factor=4", " net.inet.tcp.sendspace=1048576", " net.inet.tcp.recvspace=1048576", " net.inet.tcp.mssdflt=1300", " net.inet.tcp.slowstart_flightsize=20"]
+
+for i in ctl_var:
+    os.system(sudo + i)
+    if "=" in i:
+        eq = i.index("=")
+        print os.system("sysctl -a " + i[:eq:])
+
+
+"""
+DEFAULTS
+
+
+net.inet.tcp.win_scale_factor=3
+net.inet.tcp.sendspace=131072
+net.inet.tcp.recvspace=131072
+net.inet.tcp.mssdflt=512
+net.inet.tcp.slowstart_flightsize=1
+"""
